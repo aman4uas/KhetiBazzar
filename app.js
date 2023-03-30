64c962a5c7de552644288e5b7eb3bf0f2f286503
aman4uas <aman4u.as@gmail.com>
2023-03-30 23:01:12 +0530

    Structured the files and folder

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const app = express()
 const bodyParser = require("body-parser")
 
 app.set('view engine', 'ejs');
+const year = new Date().getFullYear();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
-    const year = new Date().getFullYear();
+    
     res.render("landingpage", {year: year})
+})
+app.get("/signup", (req, res)=>{
+    res.render("signup", {year: year})
 })
 
 app.listen(3000, ()=>{
